affce1b05c87a0c6509b237583ef14896f72aeda
Quentin Heudé <que.heude@gmail.com>
2024-04-06 16:08:55 +0200

    a jour

--- src/réserver/CalendrierAnnuel.java
+++ src/réserver/CalendrierAnnuel.java
@@ -1,4 +1,4 @@
-package réserver;
+package rÃ©server;
 
 public class CalendrierAnnuel {
 	
@@ -31,17 +31,17 @@ public class CalendrierAnnuel {
 	
 	public CalendrierAnnuel() {
 		calendrier[0] = new Mois("Janvier", 31);
-		calendrier[1] = new Mois("Février", 28);
+		calendrier[1] = new Mois("Fï¿½vrier", 28);
 		calendrier[2] = new Mois("Mars", 31);
 		calendrier[3] = new Mois("Avril", 30);
 		calendrier[4] = new Mois("Mai", 31);
 		calendrier[5] = new Mois("Juin", 30);
 		calendrier[6] = new Mois("Juillet", 31);
-		calendrier[7] = new Mois("Août", 31);
+		calendrier[7] = new Mois("Aoï¿½t", 31);
 		calendrier[8] = new Mois("Septembre", 30);
 		calendrier[9] = new Mois("Octobre", 31);
 		calendrier[10] = new Mois("Novembre", 30);
-		calendrier[11] = new Mois("Décembre", 31);
+		calendrier[11] = new Mois("Dï¿½cembre", 31);
 	}
 	
 	public boolean estLibre(int jour, int mois) {
